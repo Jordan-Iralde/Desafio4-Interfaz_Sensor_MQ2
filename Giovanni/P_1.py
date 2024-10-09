1dff67077d0724002a7f69c63f7b218e1e99e00f
Giovanni <134148500+GiovanniPeirone@users.noreply.github.com>
2024-10-09 12:01:22 -0300

    asd

--- Giovanni/P_1.py
+++ Giovanni/P_1.py
@@ -1,2 +1,14 @@
-# pip install pybluez
-# 
+#pip install bleak
+
+import asyncio
+from bleak import BleakScanner
+
+
+async def run():
+    print("Buscando dispositivos BLE...")
+    dispositivos = await BleakScanner.discover()
+    for dispositivo in dispositivos:
+        print(f"Dispositivo: {dispositivo.name}, Dirección: {dispositivo.address}")
+
+# Ejecutar la función asíncrona
+asyncio.run(run())
