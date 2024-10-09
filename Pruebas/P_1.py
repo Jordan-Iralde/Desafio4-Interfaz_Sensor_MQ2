667ae2bfea887b5caf6cea6572403f8d146eb424
Giovanni <134148500+GiovanniPeirone@users.noreply.github.com>
2024-10-09 12:54:57 -0300

    asd

--- Pruebas/P_1.py
+++ Pruebas/P_1.py
@@ -4,11 +4,10 @@ import asyncio
 from bleak import BleakScanner
 
 
-async def run():
-    print("Buscando dispositivos BLE...")
-    dispositivos = await BleakScanner.discover()
-    for dispositivo in dispositivos:
-        print(f"Dispositivo: {dispositivo.name}, Dirección: {dispositivo.address}")
+async def scan_for_devices():
+    devices = await BleakScanner.discover()
+    for device in devices:
+        print(f"Nombre: {device.name}, Dirección: {device.address}")
 
-# Ejecutar la función asíncrona
-asyncio.run(run())
+# Ejecutar el escaneo
+asyncio.run(scan_for_devices())
